96352dd9b6ecb5fe36aa6eb2641eaba38806521f
BooPassakorn <t.passakorn4999@gmail.com>
2025-02-18 16:39:07 +0700

    decorate Navigation bar

--- lib/core/widget/navigation_bar.dart
+++ lib/core/widget/navigation_bar.dart
@@ -11,30 +11,73 @@ class NavigationBarPage extends StatelessWidget {
 
     return Scaffold(
       bottomNavigationBar: Obx(
-        () => NavigationBar(
-          height: 80,
-            elevation: 0,
+        () => NavigationBarTheme( //กำหนดธีม
+          data: NavigationBarThemeData(
+            indicatorColor: Colors.transparent, //ปิด overlay
+          ),
+          child: NavigationBar(
+            backgroundColor: Colors.white,
+            height: 50, //ความสูงของแถบ
+            elevation: 0, //ไม่มีเงาข้างใต้
             selectedIndex: controller.selectedIndex.value,
             onDestinationSelected: (index) => controller.selectedIndex.value = index,
-        
+
             destinations: [
-              NavigationDestination(icon: Icon(Icons.home_outlined), label: ''),
-              NavigationDestination(icon: Icon(Icons.workspaces_outline), label: ''),
-              NavigationDestination(icon: Icon(Icons.add_circle_outline), label: ''),
-              NavigationDestination(icon: Icon(Icons.move_to_inbox_outlined), label: ''),
-              NavigationDestination(icon: Icon(Icons.account_circle_outlined), label: ''),
+              _navigationIcon(Icons.home_outlined, 0, controller),
+              _navigationIcon(Icons.workspaces_outline, 1, controller),
+              _navigationIcon(Icons.add_circle_outline, 2, controller),
+              _navigationIcon(Icons.move_to_inbox_outlined, 3, controller),
+              _navigationIcon(Icons.account_circle_outlined, 4, controller),
             ],
+          ),
         ),
       ),
       body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
+
+  NavigationDestination _navigationIcon(IconData icon, int index, NavigationController controller) {
+    bool isSelected = controller.selectedIndex.value == index;
+    return NavigationDestination(
+      icon: Padding(
+        padding: EdgeInsets.only(top: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon,
+                size: 30,
+                color: isSelected ? Color(0xff07699d) : Colors.black54),
+            if (isSelected)
+              Container(
+                margin: EdgeInsets.only(top: 2),
+                width: 20,
+                height: 2,
+                color: Color(0xff07699d),
+              ),
+          ],
+        ),
+      ),
+      label: '',
+    );
+  }
 }
 
-class NavigationController extends GetxController{
-  final Rx<int> selectedIndex = 0.obs;
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs; //เก็บค่าปัจจุบันที่ถูกเลือก
 
-  final screens = [MyHomePage(), Container(color: Colors.blueAccent,), Container(color: Colors.red,),Container(color: Colors.yellow,),Container(color: Colors.green,)];
+  final screens = [
+    MyHomePage(),
+    Container(
+      color: Colors.blueAccent,
+    ),
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.yellow,
+    ),
+    Container(
+      color: Colors.green,
+    )
+  ];
 }
-
-
