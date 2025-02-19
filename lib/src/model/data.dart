f230e4978ce535a94c8d3f4ad8714d8ba193e5dc
BooPassakorn <t.passakorn4999@gmail.com>
2025-02-19 09:02:34 +0700

    create file post_in_main.dart and data, add file image

---
+++ lib/src/model/data.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_ui_homework/src/model/post_in_main.dart';
+
+class Appdata {
+  static List<PostInMain> postInMainList = [
+    PostInMain(
+        id: 1,
+        name: "viooo",
+        nickname: "@violadwip",
+        imageProfile: "assets/story1.jpg",
+        imagePost: "assets/post1.jpg",
+        detailPost: "VALORANT Masters Bangkok 2025 กำลังจะเปิดฉากที่กรุงเทพฯ โดยมี 8 ทีมสุดแกร่งจากทั่วโลกลงประชันฝีมือ เพื่อคว้าแชมป์รายการ Masters แรกของปี 2025!",
+        isLiked: true,
+        isRecord: false),
+    PostInMain(
+        id: 2,
+        name: "aespa",
+        nickname: "@aespa_official",
+        imageProfile: "assets/profile_logo_aespa.jpg",
+        imagePost: "assets/post2.jpg",
+        detailPost: "In 2024, aespa embarked on their 2024 to 2025 ‘SYNK: Parallel Line’ world tour across Asia and Australia. After wrapping up their Asia leg this weekend in Bangkok, Thailand, they have announced new dates in North America and Europe for early 2025.",
+        isLiked: true,
+        isRecord: false),
+    PostInMain(
+        id: 3,
+        name: "NJZ",
+        nickname: "@newjeans",
+        imageProfile: "assets/profile_logo_njz.jpg",
+        imagePost: "assets/post3.jpg",
+        detailPost: 'NJZ (Korean: 엔제이지; Japanese: エンジェイジ), formerly NewJeans (뉴진스), is a five-member girl group formed by ADOR. They debuted on July 22, 2022, with the lead single "Attention", prior to the release of their debut EP New Jeans on August 1, 2022.',
+        isLiked: true,
+        isRecord: false),
+  ];
+}
---
+++ lib/src/model/post_in_main.dart
@@ -0,0 +1,20 @@
+class PostInMain {
+  int id;
+  String name;
+  String nickname;
+  String imageProfile;
+  String imagePost;
+  String detailPost;
+  bool isLiked;
+  bool isRecord;
+
+  PostInMain(
+      {required this.id,
+      required this.name,
+      required this.nickname,
+      required this.imageProfile,
+      required this.imagePost,
+      required this.detailPost,
+      required this.isLiked,
+      required this.isRecord});
+}
