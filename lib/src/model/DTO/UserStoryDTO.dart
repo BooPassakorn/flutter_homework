3acae855e1462ceea001d2c244fda39772f52d82
BooPassakorn <t.passakorn4999@gmail.com>
2025-03-18 14:33:31 +0700

    Add shared_preferences

--- lib/src/model/DTO/UserStoryDTO.dart
+++ lib/src/model/DTO/UserStoryDTO.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'dart:ffi';
 import 'dart:nativewrappers/_internal/vm/lib/typed_data_patch.dart';
+import 'package:flutter_ui_homework/constant/constant_value.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:http/http.dart' as http;
 import 'package:uuid/uuid.dart';
 
 class UserStoryDTO {
@@ -23,3 +27,18 @@ class UserStoryDTO {
     story: json['story'],
   );
 }
+
+// class userStory {
+//   static const String baseUrl = (baseURL + '/story-users');
+//
+//   static Future<List<UserStoryDTO>> fetchStories(String uuid) async {
+//     final response = await http.get(Uri.parse("$baseUrl/$uuid"));
+//
+//     if (response.statusCode == 200) {
+//       List<dynamic> jsonList = json.decode(response.body);
+//       return jsonList.map((json) => UserStoryDTO.fromJsonToUserStoryDTO(json)).toList();
+//     } else {
+//       return [];
+//     }
+//   }
+// }
